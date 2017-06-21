491bbc8e122f0c429255a2376a1f9efab433815b
Debika Sarkar <debikasarkar713@gmail.com>
2017-06-20 20:12:31 -0400

    pushing final

--- woman.js
+++ woman.js
@@ -5,48 +5,23 @@
 
 var quoteArray = [
     {
-        content: "Graphic design is building systems",
-        author: "Michael Stinson"
+        content: "The Mother of WiFi",
+        author: "Hedy Lamarr",
+              image:"https://pbs.twimg.com/media/DCir6qzXUAIsiF-.jpg"
+
     },
     {
-        content: "I rarely agree with what clients ask me to do.",
-        author: "Ross Lovegrove"
+        content: "Invented the Compiler",
+        author: "Grace Hopper",
+ image:"http://www.folioart.co.uk/images/uploads/Kiki-Ljung-Folio-Illustration-Grace-Hopper-S.jpg"
+
     },
+
+
     {
-        content: "I think most programmers spend the first 5 years of their career mastering complexity, and the rest of their lives learning simplicity.",
-        author: "Buzz Andersen"
-    },
-    {
-        content: "Make it simple. Make it memorable. Make it inviting to look at. Make it fun to read.",
-        author: "Leo Burnett"
-    },
-    {
-        content: "What makes great design great is not a trendy technique, but the logic and conceptual aspect that were figured out in the designer's mind &ndash; or on more likely, on paper &ndash; before a mouse cursor ever opened Photoshop.",
-        author: "Kyle Meyer"
-    },
-    {
-        content: "Designers tend to whisper, ad agencies tend to shout.",
-        author: " David Stuart"
-    },
-    {
-        content: "Design is the method of putting form and content together. Design, just as art, has multiple definitions; there is no single definition. Design can be art. Design can be aesthetics. Design is so simple, that's why it is so complicated.",
-        author: "Paul Rand"
-    },
-    {
-        content: "Tell it like it is, in a way they want to hear it.",
-        author: "Wihan Meerholz"
-    },
-    {
-        content: "The best way to find any and all errors is to publish your work.",
-        author: "Dan Vore"
-    },
-    {
-        content: "Creativity is piercing the mundane to find the marvelous.",
-        author: "Bill Moyers"
-    },
-    {
-        content: "I warn you against believing that advertising is a science.",
-        author: "Bill Bernbach"
+        content: "Coded us to the moon.",
+        author: "Margaret Hamilton",
+        image:"https://pbs.twimg.com/media/DCiqVNlWsAAlN0L.jpg"
     }
 ];
 
@@ -55,6 +30,7 @@ var button = document.getElementById('quote-button'),
     quote = document.getElementById('quote'),
     author = document.getElementById('quote-author'),
     tweet = document.getElementById('tweet'),
+    image = document.getElementById('image'),
     random;
 
 //Generate a Random Quote
@@ -71,7 +47,9 @@ function randomQuote (){
     //get that random quote's content
     quote.innerHTML = quoteArray[random].content;
     //get that random quote's author
-    author.innerHTML = '&mdash; ' + quoteArray[random].author;
+    author.innerHTML = quoteArray[random].author;
+   //get image
+    image.innerHTML = quoteArray[random].image;
 
     //Tweet that new generated quote
     tweetQuote();
@@ -80,6 +58,7 @@ function randomQuote (){
 //Dynamically Generate Content + Button
 function tweetQuote (){
     var quoteShort = quote.innerHTML,
+        quoteImage = image.innerHTML,
         quoteShortAuthor = author.innerHTML.substr(2).trim(),
         msg ='';
 
